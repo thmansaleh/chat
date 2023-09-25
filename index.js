fe9182c1efcb46041451bd28583bb4326a4c45b1
thmansaleh <thman.saleh@gmail.com>
2023-09-26 02:00:51 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import the 'ws' library
+import {createServer} from "http"
+import {Server} from "socket.io"
+
+
+// Create a WebSocket server
+const httpServer = createServer()
+const io = new Server(httpServer,{
+  cors:{
+    origin:"*"
+  }
+})
+io.on('connection', socket => {
+
+  socket.on('message', data => {
+    // console.log(`Received: ${data}`);
+
+    io.emit('message',` ${data}`);
+  });
+
+
+});
+
+
+httpServer.listen(3500,()=> console.log(`listen on port`))
